5fe7a626bc7ba28f6ac4f57d7a7a65d463797b1b
yibrahim9 <yousefi816@gmail.com>
2025-05-16 23:46:06 -0400

    Updates

--- src/data/guests.js
+++
@@ -1,7 +0,0 @@
-export const guests = [
-  { name: "Yousef", table: 4 },
-  { name: "Zain", table: 12 },
-  { name: "Saleh", table: 7 },
-  { name: "Sameer", table: 1 },
-  { name: "Bassem", table: 9 },
-];
